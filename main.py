8a478938019de8fc4aad76f0ffd192363e64dae6
badgeminer <64038298+badgeminer@users.noreply.github.com>
2023-04-12 15:10:29 -0600

    cgbxcgffxgg

--- main.py
+++ main.py
@@ -4,24 +4,32 @@ from pathfinding.core.grid import Grid
 from pathfinding.finder.a_star import AStarFinder
 
 matrix = [
-  [1, 1, 1, 1, 1],
-  [1, 0, 1, 1, 1],
-  [1, 1, 1, 1, 1],
-  [1, 1, 1, 1, 1],
-  [1, 1, 1, 1, 1]
+  [1, 1, 1, 1, 1, 1],
+  [1, 0, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1],
+  [0, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1]
 ]
 p = pathy.matrix(matrix)
-grid = Grid(matrix=matrix)
+grid = Grid(matrix=p.gen())
 
 
 start = grid.node(0, 0)
-end = grid.node(2, 2)
+end = grid.node(4, 5)
 
-finder = AStarFinder(diagonal_movement=DiagonalMovement.always)
-path, runs = finder.find_path(start, end, grid)
+#finder = AStarFinder(diagonal_movement=DiagonalMovement.always)
+#path, runs = finder.find_path(start, end, grid)
+#print(path)
+#print('operations:', runs, 'path length:', len(path))
+#print(grid.grid_str(path=path, start=start, end=end))
 
-print('operations:', runs, 'path length:', len(path))
-print(grid.grid_str(path=path, start=start, end=end))
+pthr = pathy.pathRUN()
+#pthRZ = pathy.pathRQ(grid,start,end)
+#pthr.queue.put(pthRZ)
+pthRz = pathy.pathRQ(grid,start,grid.node(*p.most))
+pthr.queue.put(pthRz)
 
 pygame.init()
 scr = pygame.display.set_mode((500,500))
@@ -30,12 +38,20 @@ font = pygame.font.Font(None, 12)
 
 while True:
     scr.fill((0,0,0))
+    
 
-    for x in range(len(matrix)):
-        for y in range(len(matrix[x])):
-            if matrix[x][y]:
-                scr.fill((255,255,255),(x*20,y*20,20,20))
-            scr.blit(font.render(str(p.mtrxdz[x][y]),True,(255,255,255),(0,0,0)),((x*20)+1,(y*20)+1))
+
+    for y in range(len(matrix)):
+        for x in range(len(matrix[y])):
+            if matrix[y][x]:
+                scr.fill(p.calc(x,y),(x*20,y*20,20,20))
+            scr.blit(font.render(str(p.mtrxdz[y][x]),True,(255,255,255),(0,0,0)),((x*20)+1,(y*20)+1))
+    if pthRz.st == pathy.rqState.done:
+        prv = (10,10)
+        for i in pthRz.pth: 
+            i = ((i[0]*20)+10,(i[1]*20)+10)
+            pygame.draw.line(scr,(255,0,0),prv,i)
+            prv = i
 
     pygame.display.flip()
     for e in pygame.event.get():
--- pathy.py
+++ pathy.py
@@ -1,21 +1,87 @@
 import numpy as np
-import pygame
+import pygame,threading,queue,enum
+from pathfinding.core.diagonal_movement import DiagonalMovement
+from pathfinding.core.grid import Grid
+from pathfinding.finder.a_star import AStarFinder
+
+class rqState(enum.Enum):
+    waiting = 0
+    in_progress = 1
+    done = 2
+
+mx = pygame.Color(0,255,0)
+mn = pygame.Color(0,0,255)
+
+class pathRQ:
+    def __init__(self,grid:Grid,s,e) -> None:
+        self.st = rqState.waiting
+        self.g  = grid
+        self.g.cleanup()
+        self.s,self.e = s,e
+        self.pth = None
+
+class pathRUN:
+    def __init__(self) -> None:
+        self.thrd =threading.Thread(None,self.run,daemon=True)
+        self.queue = queue.Queue()
+        self.thrd.start()
+    def run(self):
+        while True:
+            r:pathRQ = self.queue.get()
+            r.st = rqState.in_progress
+            finder = AStarFinder(diagonal_movement=DiagonalMovement.only_when_no_obstacle)
+            path, runs = finder.find_path(r.s, r.e, r.g)
+            print('operations:', runs, 'path length:', len(path))
+            print(r.g.grid_str(path=path, start=r.s, end=r.e))
+            r.pth = path
+            r.st = rqState.done
+
+
 class matrix:
     def __init__(self,mtrx) -> None:
         self.mtrx = mtrx
         self.mtrxdz = []
+        self.most = (0,0)
+        self.mv = 0
+
         self.dist()
+        self.calcd = {}
+    def calc(self,x,y):
+        if (x,y) in self.calcd:
+            return self.calcd[(x,y)]
+        d = self.mtrxdz[y][x]
+        dc = (d/self.mv)
+        if dc > 1: dc = 1
+        try:
+            C = mn.lerp(mx,dc)
+        except:
+            C = mn
+        self.calcd[(x,y)] = C
+        return C
+    def gen(self):
+        g = []
+        for r in range(len(self.mtrx)):
+            R = []
+            for i in range(len(self.mtrx[r])):
+                R.append(self.mtrxdz[r][i])
+            g.append(R)
+        return g
+
+
+
 
     def dist(self):
         print(self.mtrx)
-        for X in range(len(self.mtrx)):
+        most = 0
+        mpos = (-1,-1)
+        for Y in range(len(self.mtrx)):
             R = []
-            for Y in range(len(self.mtrx[X])):
-                if self.mtrx[X][Y] >= 1:
+            for X in range(len(self.mtrx[Y])):
+                if self.mtrx[Y][X] >= 1:
                     dzt = 100.0
-                    for x in range(len(self.mtrx)):
-                        for y in range(len(self.mtrx[x])):
-                            if (self.mtrx[x][y] >= 1): 
+                    for y in range(len(self.mtrx)):
+                        for x in range(len(self.mtrx[y])):
+                            if (self.mtrx[y][x] >= 1): 
                               pass
                             else:
                                 d = pygame.math.Vector2(X, Y).distance_to((x,y))
@@ -25,7 +91,12 @@ class matrix:
                                 else: 
                                   print(d)
                 else:
-                    print(self.mtrx[X][Y])
+                    print(self.mtrx[Y][X])
                     dzt = 0.0
+                if dzt > most:
+                    most = dzt
+                    mpos = (X,Y)
                 R.append(round(dzt*10)/10)
             self.mtrxdz.append(R)
+        self.most = mpos
+        self.mv = most
